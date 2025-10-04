65d525fb422ad7c67b63af76872101aae814fad1
Guillaume Everarts de Velp <edvgui@gmail.com>
2025-10-04 12:27:51 +0200

    fixes

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,5 +1,5 @@
 """
-Copyright Guillaume Everarts de Velp 2025
+Copyright 2025 Guillaume Everarts de Velp
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
